b142743b27a4966a0497eff6c1deb42c73501aed
moritzgladigau <moritz.gladigau@gmail.com>
2023-12-19 20:43:03 +0100

    Start over

--- main.c
+++ main.c
@@ -10,7 +10,6 @@
 
 /* Hier wird das Spiel erklärt. */
 void spiel_anleitung();
-int user_input(char w[]);
 void flush();
 
 int main()
@@ -24,16 +23,6 @@ int main()
         printf("Hallo hier wird Minesweeper gespielt :)\n");
         spiel_anleitung();
 
-        do{
-                result = user_input(s);
-                if (result == SUCCSES) {
-                        printf("Input is valid.\n");
-                } else {
-                        system("clear");
-                        printf("ERROR: Wrong Input!!\nGebe <help> ein um mehr zu erfahren.\n");
-                }
-        } while (result == ERROR);
-        
         return 0;
 }
 
@@ -46,51 +35,6 @@ void spiel_anleitung()
         printf("||\t\t\t\t\t\t\t\t\t\t||\n||\t----> Gebe <help> ein um wieder zu diesr Anleitung zu kommen <----\t||\n");
         printf("==================================================================================\n");
         printf("\n");
-}
-
-int user_input(char input[])
-{
-        /* Der User darf <help>, Feldcoordinaten z.B: <B1>, Flagen/Aufdeken z.B: <a> oder beides <B1a> */
-        size_t len;
-
-        printf("Deine Eingabe: ");
-        scanf("%s", input);
-
-        if (strcmp(input, "help") == 0) {
-                /* Gebe die Anleitung wieder aus */
-                printf("Anleitung");
-                spiel_anleitung();
-                return ERROR;
-        } 
-
-        len = strlen(input);
-
-        /*      
-        uberpfuefe ob der wert Drei stellen oder weniger hat <B1f> 
-        wenn der wert nur eine stelle hatt muss diese ein kleinbuchstabe sein <f> / <a> 
-        wenn der wert Zweistellen hat oder mehr muss der erste ein Grosbuchtstabe <A> der zweite eine Zahl <1> sein
-        hat der wert drei stellen muss vorheriges gelten und der dritte wert muss ein kleinbustabe sein!! 
-        */
-
-        if (len <= 3) {
-                printf("Der Wert hat drei Stellen oder weniger <B1f>\n");
-                if (len == 1 && islower(input[0])) {
-                        /* printf("EinKle\n"); */
-                        if (input[0] == 'a' || input[0] == 'f'){
-                                return SUCCSES;
-                        } 
-                } else if (len >= 2 && isupper(input[0]) && isdigit(input[1]) && (len == 2 || (len == 3 && islower(input[2])))) {
-                        /* printf("Paast\n"); */
-                        if (len == 3 && (input[2] != 'a' || input[2] != 'f')){
-                                return ERROR;
-                        }
-                        return SUCCSES;
-                }
-        }
-        /* printf("NOOOO u go WRONG!!!!!"); */
-        
-        flush();
-        return ERROR;
 }
 
 void flush()
