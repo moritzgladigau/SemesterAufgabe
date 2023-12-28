ecbf5d8dd7bce1825f65fae2f1616a0dd6513b09
Ben <wolle.webdesign@gmail.com>
2023-12-28 22:27:53 +0100

    Tests und überlegungen

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
-/*Moritz ist doof*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void)
+{
+        printf("Test");
+        return 0;
+}
+
+/*To-Do: 1. Feld initalisieren und Speicherplatz sichern.
+         2. Feld zufällig mit Minen befüllen 
+         3. Aufgedecktes Feld mit seinen Nachbarn vergleichen und die Anzahl in das Feld schreiben
+         game-of-life.c!!!*/
