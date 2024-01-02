e64baa98fc108afb3f18e115bb4f6f202f32f90c
Ben <wolle.webdesign@gmail.com>
2024-01-02 12:17:27 +0100

    mines_nearby

--- main.c
+++ main.c
@@ -3,19 +3,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <float.h>
+#define MINE 'M'
+#define BLANK ' '
+#define FILED_SIZE 10
 
-
-double percent_mines = 9.5;
+double percent_mines = 10;
 
 char **field_init(int width, int height);
 int fill_field(char **field, int width, int height);
 char random_state(double mine_percent);
 void print_field(char **field, int width, int height);
+int mines_nearby(char **field, int width, int height, int x, int y);
 
 int main(void)
 {
         char **field;
-        int width = 5, height = 5;
+        int width = FILED_SIZE, height = FILED_SIZE, user_x = 3, user_y = 4;
         srand(time(NULL));
         field = field_init(width, height);
         fill_field(field, width, height);
@@ -53,6 +56,30 @@ int fill_field(char **field, int width, int height)
                 }
                 
         }
+        for (i = 0; i < height; i++)
+        {
+                for (j = 0; j < width; j++)
+                {
+                        if (field[i][j] != MINE)
+                        {
+                                field[i][j] = (mines_nearby(field, width, height, i, j) + '0');
+                        }
+                        
+                }
+                
+        }
+        for (i = 0; i < height; i++)
+        {
+                for (j = 0; j < width; j++)
+                {
+                        if (field[i][j] == '0')
+                        {
+                                field[i][j] = ' ';
+                        }
+                        
+                }
+                
+        }
         return 1;
 }
 
@@ -62,11 +89,9 @@ char random_state(double mine_percent)
         int alive = mine_percent;
         if (alive >= state)
         {
-                return 'M';
-        }else {
-                return '0';
+                return MINE;
         }
-
+        return BLANK;
 }
 
 void print_field(char **field, int width, int height)
@@ -87,6 +112,23 @@ void print_field(char **field, int width, int height)
                 printf("\n");
                 
         }
+}
+
+int mines_nearby(char **field, int width, int height, int x, int y)
+{
+    int mine_count = 0;
+    int i, neighbours_x = 0, neighbours_y = 0;
+    int neighbours[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    for (i = 0; i < 8; i++)
+    {
+        neighbours_x = x + neighbours[i][0];
+        neighbours_y = y + neighbours[i][1];
+        if (neighbours_x >= 0 && neighbours_x < height && neighbours_y >= 0 && neighbours_y < width && field[neighbours_x][neighbours_y] == MINE)
+        {
+            mine_count++;
+        }
+    }
+    return mine_count;
 }
 
 /*To-Do: 1. Feld initalisieren und Speicherplatz sichern.
